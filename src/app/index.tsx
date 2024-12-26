d952379215cc531d9e62b998a86fab948a1e56c2
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-12-26 20:48:41 -0300

    feat: create Home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,8 @@ import { Steps } from '../components/steps';
 import { Welcome } from '../components/welcome';
 import { Button } from '@/components/button';
 
+import { router } from 'expo-router'
+
 export default function Index() {
   const { title, subtitle } = welcome;
   return (
@@ -21,7 +23,7 @@ export default function Index() {
       <Steps />
 
       <View style={{ alignItems: 'center' }}>
-        <Button>
+        <Button onPress={() => router.navigate('/home')}>
           <Button.Title>Começar</Button.Title>
         </Button>
       </View>
